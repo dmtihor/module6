b7bb0416706f45198610fbce85d102ac858a1b8b
dmtihor <dmtihor@gmail.com>
2015-12-23 16:56:59 +0200

    v4

--- src/com/dmtihor/homework2/FlowersBouguet.java
+++ src/com/dmtihor/homework2/FlowersBouguet.java
@@ -6,5 +6,17 @@ import java.util.ArrayList;
  * Created by Adm on 12/22/2015.
  */
 public class FlowersBouguet {
-    ArrayList<Flower> flowers = new ArrayList();
+    public static void main(String[] args) {
+        try {
+            ArrayList<Flower> flowers = new ArrayList();
+            if (flowers.size() % 2 == 0) {
+                throw new NotGoodBouguet();
+            }
+
+        } catch (IllegalStateException ilse) {
+            System.out.println("IllegalStateException");
+        } catch (NotGoodBouguet ngb) {
+            System.out.println("Bouquet for funerals !!!");
+        }
+    }
 }
